956621d1d071ef8a2c9d75a253162bf512ca0505
Yurii Koltso <yurkoga25@gmail.com>
2019-05-09 16:59:49 +0300

    Responsive layout

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## How to start?
 
 - Download or clone this repository: `git clone https://github.com/yugako/upqode-test.git`
-- Go to the repository folder: `cd upqode-test`
+- Open terminal and go to the repository folder: `cd upqode-test`
 - Run: `npm install`, then `gulp`
--- app/js/common.js
+++ app/js/common.js
@@ -6,4 +6,31 @@ $(function() {
 		}, 2000);
     });
 
+    $('.banner-slider').slick({
+    	autoplay: true,
+    	autoplaySpeed: Math.floor(Math.random()*2000 + 5000),
+    	dots: true,
+    	prevArrow: '<button class="banner-slider_prev banner-buttons">&lt;</button>',
+    	nextArrow: '<button class="banner-slider_next banner-buttons">&gt;</button>',
+    	customPaging : function(slider, i) {
+    	 var thumb = $(slider.$slides[i-1]).data();
+    	  return '<span class="dot"></span>';
+    	 },
+    });
+
+    $('.products-slider').slick({
+    	autoplay: false,
+    	dots: true,
+    	arrows: false,
+    	customPaging : function(slider, i) {
+    	 var thumb = $(slider.$slides[i-1]).data();
+    	  return '<span class="dot"></span>';
+    	 },
+    });
+
+    $('.burger').click(function () {
+    	$('.burger span').toggleClass('active');
+    	$('.menu').toggleClass('menu-active');
+    })
+
 });
